55b97f8e720867639ec13397b2b2fc963cf2e12e
PaulCrtr <paul.th.cartier@gmail.com>
2020-04-02 12:43:25 +0200

    function Ok

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,12 +1,22 @@
 function capitalizeFirstLetters(str) {
-    return str.split(' ').map(x=>x[0].toLowerCase())
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[0] || str[i - 1] === " ") {
+      newStr = newStr.concat(str[i].toUpperCase());
+    } else {
+      newStr = newStr.concat(str[i]);
+    }
+  }
+  return newStr;
 }
 
-
-const assert = require('assert')
+const assert = require("assert");
 
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 assert.strictEqual(capitalizeFirstLetters("javaScript"), "JavaScript");
 assert.strictEqual(capitalizeFirstLetters(""), "");
-assert.strictEqual(capitalizeFirstLetters("bonjour monsieur ok"), "Bonjour Monsieur Ok");
+assert.strictEqual(
+  capitalizeFirstLetters("bonjour monsieur ok"),
+  "Bonjour Monsieur Ok"
+);
